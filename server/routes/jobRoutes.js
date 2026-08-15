1a584abd0d1304b5f9112e2ca2ac07770fad0437
Ananjana <ananjanak246@gmail.com>
2026-08-15 23:22:21 +0530

    Add role based authorization

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { createJob } = require("../controllers/jobController");
 const protect = require("../middleware/authMiddleware");
+const authorizeRoles = require("../middleware/roleMiddleware");
 
 const router = express.Router();
 
-router.post("/", protect, createJob);
+router.post("/", protect, authorizeRoles("recruiter","admin"),createJob);
 
 module.exports = router;
